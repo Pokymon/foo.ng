c50a9e88320dd3e5066e90882c1bf475331a2c33
Kamsi Okoro <theofficialkamsiokoro@gmail.com>
2026-08-18 00:14:44 +0100

    Simplify vite config, fix build

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,8 @@
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
-import {fileURLToPath} from 'url';
-import path from 'path';
 import {defineConfig} from 'vite';
 
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
-
-export default defineConfig(({ command, mode }) => {
-  return {
-    base: mode === 'production' || command === 'build' ? './' : '/',
-    plugins: [react(), tailwindcss()],
-    resolve: {
-      alias: {
-        '@': path.resolve(__dirname, '.'),
-      },
-    },
-    server: {
-      hmr: process.env.DISABLE_HMR !== 'true',
-      watch: process.env.DISABLE_HMR === 'true' ? null : {},
-    },
-  };
+export default defineConfig({
+  base: './',
+  plugins: [react(), tailwindcss()],
 });
